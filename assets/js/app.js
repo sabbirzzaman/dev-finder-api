3f3ea1cf15f781d2fa97fe07d534533c3455fa8b
sabbirzzaman <sztonmoy7@gamil.com>
2022-03-02 23:43:44 +0600

    error added for null and undefined value

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,49 +38,64 @@ const displayData = (data) => {
     const company = selectElement('company');
 
     // Set attribute
-    userImage.setAttribute('src', data.avatar_url)
+    userImage.setAttribute('src', data.avatar_url);
 
     // Modified information
-    const blogUrl = data.blog.slice(8);
-    const userAddress = data.location.split(',')[1];
+    const blogUrl = data?.blog?.slice(8);
+    const userAddress = data?.location?.split(',')[1];
+    const dateTime = new Date(data.created_at).toDateString().slice(4);
 
     // Set innerText
-    name.innerText = data.name;
-    join.innerText = data.created_at;
-    username.innerText = data.login;
-    bio.innerText = data.bio;
+    !data.name ? (name.innerText = 'Not Set') : (name.innerText = data.name);
+    !data.created_at
+        ? (join.innerText = 'No Date')
+        : (join.innerText = dateTime);
+    !data.login
+        ? (username.innerText = 'No Username')
+        : (username.innerText = data.login);
+    !data.bio
+        ? (bio.innerText = 'This profile has no username.')
+        : (bio.innerText = data.bio);
+
     repo.innerText = data.public_repos;
     followers.innerText = data.followers;
     following.innerText = data.following;
-    address.innerText = userAddress;
-    twitter.innerText = data.twitter_username;
-    link.innerText = blogUrl.slice(0, -1)
-    company.innerText = data.company;
+
+    !data.location || dateTime.location === undefined
+        ? (address.innerText = 'Not Set')
+        : (address.innerText = userAddress);
+    !data.twitter
+        ? (twitter.innerText = 'Not Set')
+        : (twitter.innerText = data.twitter);
+    !data.blog ? (link.innerText = 'Not Set') : (link.innerText = blogUrl);
+    !data.company
+        ? (company.innerText = 'Not Set')
+        : (company.innerText = data.company);
 };
 
 themeToggle.addEventListener('click', () => {
     const toggleContainer = document.getElementById('theme-toggle');
     const h2 = document.getElementsByTagName('h2');
 
-    if(toggleContainer.firstChild.innerText === 'Light') {
+    if (toggleContainer.firstChild.innerText === 'Light') {
         toggleContainer.textContent = '';
-        toggleContainer.innerHTML = `<span>Dark</span> <i class="fa-solid fa-moon"></i>`
-        for(element of h2) {
-            element.style.color = '#000'
+        toggleContainer.innerHTML = `<span>Dark</span> <i class="fa-solid fa-moon"></i>`;
+        for (element of h2) {
+            element.style.color = '#000';
         }
     } else {
         toggleContainer.textContent = '';
-        toggleContainer.innerHTML = `<span>Light</span> <i class="fa-solid fa-sun"></i>`
-        for(element of h2) {
-            element.style.color = '#fff'
+        toggleContainer.innerHTML = `<span>Light</span> <i class="fa-solid fa-sun"></i>`;
+        for (element of h2) {
+            element.style.color = '#fff';
         }
     }
 
     const body = document.body;
-    const header = document.querySelector('.header')
-    const searchBar = document.querySelector('.search-dev')
-    const userInfo = document.querySelector('.user')
-    const otherInfo = document.querySelector('.other-info')
+    const header = document.querySelector('.header');
+    const searchBar = document.querySelector('.search-dev');
+    const userInfo = document.querySelector('.user');
+    const otherInfo = document.querySelector('.other-info');
     body.classList.toggle('light-bg');
     header.classList.toggle('light-header');
     searchBar.classList.toggle('light-search-bar');
