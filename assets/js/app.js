b19da272a2c816a3dabcad014b52f5a7c39ec86f
sabbirzzaman <sztonmoy7@gamil.com>
2022-03-02 19:17:37 +0600

    display dynanic content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,58 @@
-console.log(`https://api.github.com/users/Aoussiad-Mehdi`)
+// Select elements
+const selectElement = (id) => {
+    const element = document.getElementById(id);
+    return element;
+};
+
+// Select global elements
+const searchInput = selectElement('search-input');
+const searchBtn = selectElement('search-btn');
+
+// Search button event handler
+loadData = async () => {
+    const keyword = searchInput.value;
+    const url = `https://api.github.com/users/${keyword}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    displayData(data);
+};
+
+const displayData = (data) => {
+    searchInput.value = '';
+
+    // Select elements
+    const userImage = selectElement('user-img');
+    const name = selectElement('name');
+    const join = selectElement('join');
+    const username = selectElement('username');
+    const bio = selectElement('bio');
+    const repo = selectElement('repo');
+    const followers = selectElement('followers');
+    const following = selectElement('following');
+    const address = selectElement('address');
+    const twitter = selectElement('twitter');
+    const link = selectElement('link');
+    const company = selectElement('company');
+
+    // Set attribute
+    userImage.setAttribute('src', data.avatar_url)
+
+    // Modified information
+    const blogUrl = data.blog.slice(8);
+    const userAddress = data.location.split(',')[1];
+
+    // Set innerText
+    name.innerText = data.name;
+    join.innerText = data.created_at;
+    username.innerText = data.login;
+    bio.innerText = data.bio;
+    repo.innerText = data.public_repos;
+    followers.innerText = data.followers;
+    following.innerText = data.following;
+    address.innerText = userAddress;
+    twitter.innerText = data.twitter_username;
+    link.innerText = blogUrl.slice(0, -1)
+    company.innerText = data.company;
+};
